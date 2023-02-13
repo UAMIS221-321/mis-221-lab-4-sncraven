0823bfc1a44ada40454e91200c5e37e3198af848
Stacie Craven <stacie@Stacies-MacBook-Pro.local>
2023-02-13 11:21:56 -0600

    lab 4

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string Full = GetFull();
+string Partial = GetPartial();
+
+static string GetFull() {
+    System.Console.WriteLine("how man rows are there");
+    return Console.ReadLine();
+        
+}
