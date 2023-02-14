3fa82f85df92b7da08febdb803675eac018ece81
Stacie Craven <stacie@Stacies-MacBook-Pro.local>
2023-02-13 22:15:26 -0600

    lab 4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿//System.Console.WriteLine("enter a number");
+﻿
+//System.Console.WriteLine("enter a number");
 //int count = int.Parse(Console.ReadLine());
 //for (int i=0; i<=count; i++) {
 //    for (int j=0; j<=i; j++) {
@@ -7,16 +8,17 @@
 //    Console.Write("\n");
 //}
 
-Random rnd = new Random();
+
 int number = rnd.Next();
 System.Console.WriteLine("enter a number");
 int count = int.Parse(Console.ReadLine());
 for (int i=0; i<=count; i++) {
-    for (int j=0; j<=i; j++) {
+    for (int j=0; j<=i; j++) { 
         System.Console.WriteLine(rnd.Next(2));
         Console.Write("*");
     }
     Console.Write("\n");
 }
+
 
     
