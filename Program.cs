17eccb6afc8a29cf4787bafe06c80135179bc104
Stacie Craven <stacie@Stacies-MacBook-Pro.local>
2023-02-15 11:01:01 -0600

    lab 4

--- Program.cs
+++ Program.cs
@@ -1,22 +1,34 @@
-﻿
-System.Console.WriteLine("enter a number");
-int count = int.Parse(Console.ReadLine());
-for (int i=0; i<=count; i++) {
-    for (int j=0; j<=i; j++) {
-       Console.Write("o");
+﻿GetFull();
+GetPartial();
+
+static void GetFull() {
+    System.Console.WriteLine("enter a number 3-9");
+    int count = int.Parse(Console.ReadLine());
+    for (int i=0; i<= count; i++) {
+        for (int j=0; j<=i; j++) {
+        Console.Write("o");
+        }
+        Console.Write("\n");
     }
-    Console.Write("\n");
 }
 
-Random rnd = new Random();
-int number = rnd.Next();
-for (int i=0; i<= count; i++) {
-    for (int j=0; j<=i; j++) { 
-        System.Console.WriteLine(rnd.Next(2));
-        Console.Write("o");
+
+
+static void GetPartial() {
+    Random rnd = new Random();
+    int number = rnd.Next(3,9);
+    for (int i = 0; i < number; i++) {
+        for (int j = 0; j <= i; j++) { 
+            int choice = rnd.Next(2);
+            if (choice == 0) {
+            Console.Write("o");
+            }
+        }
+        Console.Write("\n");
     }
-    Console.Write("\n");
+        
 }
+    
 
 
     
